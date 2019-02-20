b34609c8fff04a6eec199ea09f7c7bc4197eb5f7
Fedek007 <fedekkrut@mail.ru>
2019-02-20 14:38:17 +0300

    Только с кнопками дз

---
+++ Buttons/app/src/main/java/com/example/buttons/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.buttons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
